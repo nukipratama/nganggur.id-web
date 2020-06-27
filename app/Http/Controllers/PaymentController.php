c41d1859ec07945e03f4208677dc686ef4163ec6
nukipratama <nukipratamaa@gmail.com>
2020-06-27 14:48:30 +0700

    withdraw

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -73,6 +73,13 @@ class PaymentController extends Controller
     {
         $project->withdraw_at = now();
         $project->save();
+        Notification::create([
+            'user_id' => $project->partner_id,
+            'title' => 'Permintaan Pencairan Dana',
+            'description' => 'Permintaan Pencairan Dana untuk ' . $project->title . ' sedang diproses. Silahkan menunggu konfirmasi dari pihak Nganggur.id.',
+            'icon' => $project->subtype->icon,
+            'target' => route('project.details', ['project' => $project->id]),
+        ]);
         toast('Berhasil meminta pencairan dana<br>Permintaan Pencairan Dana akan segera diproses', 'success');
         session()->flash('home', route('home'));
         return redirect(route('project.details', ['project' => $project->id]));
--- resources/views/project/status/partnerPayment.blade.php
+++ resources/views/project/status/partnerPayment.blade.php
@@ -6,20 +6,37 @@
             </div>
             <div class="col-12 my-1">
                 <div class="row">
-                    @if ($project->withdraw_at)
+                    @if ($project->withdraw_at && !$project->withdraw_verified_at)
                     <div class="col-12">
-                        <p class="font-weight-bold">Anda telah meminta pencairan dana.
-                            <br>Selanjutnya, silahkan menunggu konfirmasi pihak Nganggur.id terkait pencairan dana.
+                        <p class="lead text-justify">Anda telah meminta pencairan dana. Selanjutnya, silahkan menunggu
+                            konfirmasi pihak Nganggur.id terkait pencairan dana.
+                    </div>
+                    @endif
+                    @if ($project->withdraw_at && $project->withdraw_verified_at)
+                    <div class="row align-items-center justify-content-center m-3">
+                        <div class="col-md-6">
+                            <img src="{{asset('img/withdraw.png')}}" class="img-fluid">
+                        </div>
+                        <div class="col-md-6">
+                            <p class="lead text-justify">
+                                Pencairan dana senilai @currency($project->withdraw['nominal']) telah dikirimkan ke
+                                {{$project->partner->details->bank->name}} -
+                                {{$project->partner->details->bank_account}} a.n
+                                {{$project->partner->details->bank_account_name}} pada tanggal
+                                {{\Carbon\Carbon::parse($project->withdraw_verified_at)->format('d M Y')}}.
+                        </div>
                     </div>
                     @endif
                     @if (!$project->partner->details->bank_id || !$project->partner->details->bank_account ||
-                    !$project->partner->details->bank_account_name)
+                    !$project->partner->details->bank_account_name )
                     <div class="col-12">
-                        <p class="font-weight-bold">Anda belum melengkapi informasi rekening.
+                        <p class="lead text-justify">Anda belum melengkapi informasi rekening.
                             <br><a href="">Klik disini</a> untuk melengkapi informasi rekening.
                             <br>Informasi Rekening digunakan sebagai tujuan pencairan dana project oleh Nganggur.id.</p>
                     </div>
-                    @else
+                    @endif
+                    @if (!$project->partner->details->bank_id || !$project->partner->details->bank_account ||
+                    !$project->partner->details->bank_account_name || !$project->withdraw_verified_at)
                     <div class="col-12 text-center">
                         <p class="font-weight-bold text-left">Informasi Rekening Pembayaran :</p>
                         <h1 class="display-4 font-weight-bold">{{$project->partner->details->bank_account}}</h1>
