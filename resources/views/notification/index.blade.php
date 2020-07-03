784c05c3c657dcd872680c9e03d8644450f4a407
nukipratama <nukipratamaa@gmail.com>
2020-07-03 21:12:27 +0700

    100

--- resources/views/notification/index.blade.php
+++ resources/views/notification/index.blade.php
@@ -41,7 +41,9 @@
             </div>
         </div>
     </div>
-
+    <div class="row justify-content-center mt-3">
+        {{ $notification->links() }}
+    </div>
     <script>
         function request() {
             function dot(count) {
@@ -58,7 +60,7 @@
                 success: function (data) {
                     data.forEach(element => {
                         var time = new Date(element['created_at']);
-                    var d = time.getDate() + "/" + (time.getMonth()+1);
+                        var d = time.getDate() + "/" + (time.getMonth() + 1);
                         var content = `<a href="` + element['target'] + `" class="text-dark ">
             <div class="row justify-content-center align-items-center cardRipple rounded">
             <div class="col-3 col-md-1">
@@ -66,7 +68,7 @@
             </div>
             <div class="col-9 col-md-11 pl-0">
                ` + dot(element['read']) + `
-            <p class="pl-1 text-secondary float-right">` +d.toString() + `</p>
+            <p class="pl-1 text-secondary float-right">` + d.toString() + `</p>
             <p class="font-weight-bold my-0">` + element['title'] + `</p>
             <p class="text-secondary my-0">` + element['description'] + `</p>
             </div>
