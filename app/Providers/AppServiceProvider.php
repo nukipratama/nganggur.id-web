d1c14559e22038e1aa5c8d21db15bc368929f293
Nuki Pratama <nukipratamaa@gmail.com>
2024-08-03 21:22:43 +0700

    cleanup AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Observers\UserObserver;
 use App\User;
 use Blade;
-use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,9 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        config(['app.locale' => 'id']);
-        date_default_timezone_set('Asia/Jakarta');
-        Carbon::setLocale('id');
         Blade::directive('currency', function ($expression) {
             return "Rp <?php echo number_format($expression, 0, ',', '.'); ?>";
         });
